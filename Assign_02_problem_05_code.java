4fc2cc14b4ed9ec5dbea244b589cc984808195a0
Ahmed-Rafat6628 <130044155+Ahmed-Rafat6628@users.noreply.github.com>
2023-04-30 00:00:29 +0200

    Update Assign_02_problem_05_code.java

--- Assign_02_problem_05_code.java
+++ Assign_02_problem_05_code.java
@@ -24,38 +24,3 @@ System.out.println("Not");
 }       
 }
 }
-
-
-
-
-
-
-
-
-import java.util.Scanner;
-public class Main {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-int t = sc.nextInt(); // number of test cases
-while (t-- > 0) {
-int x = sc.nextInt(); // input number
-boolean isPrime = true;
-if (x <= 1) {
-isPrime = false;
-            }
-for (int i = 2; i <= x/2; i++) {
-if (x % i == 0) {
-isPrime = false;
-break;
-}
-}          
-if (isPrime) 
- System.out.println("Prime");
- else 
-System.out.println("Not");
- 
-}
-        
- 
-    }
-}
