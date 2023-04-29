d3d92dc6eff161f68a421dda00ba6ff5e2f7f249
Ahmed-Rafat6628 <130044155+Ahmed-Rafat6628@users.noreply.github.com>
2023-04-30 00:07:39 +0200

    Update and rename Assign_04_problem_03_code.txt to Assign_04_problem_03_code.java

---
+++ Assign_04_problem_03_code.java
@@ -0,0 +1,27 @@
+//Ahmed Rafat Abdelrehem
+//20216628
+import java.util.Scanner;
+ 
+public class Assignment4 {
+    static int Search(int[] arr, int sz, int target) {
+        for (int i = 0; i < sz; i++)
+        {
+            if (arr[i] == target) return i;
+        }
+        return -1;
+    }
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sz = sc.nextInt();
+        int[] arr = new int[sz];
+        for (int i = 0; i < sz; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int res = Search(arr, sz, target);
+        if (res == -1) System.out.println("-1");
+        else System.out.println(res);
+    }
+}
